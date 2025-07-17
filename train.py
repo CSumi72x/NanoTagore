e58528a35c0ac6c91b7a57c40b9defafc3171322
CSumi72x <sumichasa72@gmail.com>
2025-07-18 00:11:23 +0700

    train.py

--- train.py
+++ train.py
@@ -299,25 +299,25 @@ while True:
             })
       if losses['val'] < best_val_loss or always_save_checkpoint:
         best_val_loss = losses['val']
-            if iter_num > 0:
-                checkpoint = {
-                    'model': raw_model.state_dict(),
-                    'optimizer': optimizer.state_dict(),
-                    'model_args': model_args,
-                    'iter_num': iter_num,
-                    'best_val_loss': best_val_loss,
-                    'config': config,
+        if iter_num > 0:
+            checkpoint = {
+                'model': raw_model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'model_args': model_args,
+                'iter_num': iter_num,
+                'best_val_loss': best_val_loss,
+                'config': config,
                 }
-                checkpoint_dir = '/kaggle/working/'
-                checkpoint_path_latest = os.path.join(checkpoint_dir, 'checkpoint_latest.pt')
-                checkpoint_path_named = os.path.join(checkpoint_dir, f'checkpoint_{iter_num}.pt')
+            checkpoint_dir = '/kaggle/working/'
+            checkpoint_path_latest = os.path.join(checkpoint_dir, 'checkpoint_latest.pt')
+            checkpoint_path_named = os.path.join(checkpoint_dir, f'checkpoint_{iter_num}.pt')
 
 # save checkpoint
-                 torch.save(checkpoint, checkpoint_path_latest)
-                 torch.save(checkpoint, checkpoint_path_named)
-                 torch.save(checkpoint, os.path.join(out_dir, 'checkpoint_latest.pt'))
-                 torch.save(checkpoint, os.path.join(out_dir, f'checkpoint_{iter_num}.pt'))
-                 print(f"✅ Saved checkpoint at iteration {iter_num} (val loss = {best_val_loss:.4f})")
+             torch.save(checkpoint, checkpoint_path_latest)
+             torch.save(checkpoint, checkpoint_path_named)
+             torch.save(checkpoint, os.path.join(out_dir, 'checkpoint_latest.pt'))
+             torch.save(checkpoint, os.path.join(out_dir, f'checkpoint_{iter_num}.pt'))
+             print(f" Saved checkpoint at iteration {iter_num} (val loss = {best_val_loss:.4f})")
     if iter_num == 0 and eval_only:
         break
 
